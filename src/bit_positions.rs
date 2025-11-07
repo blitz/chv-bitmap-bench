c89d6ccdf663882a26be56fdbc4c69fc79538844
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2025-11-07 10:59:26 +0100

    Remove recursion
    Signed-off-by: Julian Stecklina <julian.stecklina@cyberus-technology.de>

--- src/bit_positions.rs
+++ src/bit_positions.rs
@@ -17,35 +17,30 @@ where
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current_word.is_none() {
-            self.current_word = self.underlying_it.next().map(|w| (w, 0));
-        }
+        loop {
+            if self.current_word.is_none() {
+                self.current_word = self.underlying_it.next().map(|w| (w, 0));
+            }
 
-        if let Some((word, word_bit)) = self.current_word {
-            let shifted_word = if word_bit < 64 { word >> word_bit } else { 0 };
+            let (word, word_bit) = self.current_word?;
+            if word != 0 {
+                let shifted_word = if word_bit < 64 { word >> word_bit } else { 0 };
 
-            if shifted_word == 0 {
-                self.current_word = None;
-                self.word_pos += 1;
-                self.next()
-            } else {
-                let zeroes = shifted_word.trailing_zeros();
+                if shifted_word != 0 {
+                    let zeroes = shifted_word.trailing_zeros();
 
-                assert!(zeroes + word_bit <= 64);
-                if zeroes + word_bit == 64 {
-                    self.current_word = None;
-                    self.word_pos += 1;
-                    self.next()
-                } else {
-                    self.current_word = Some((word, zeroes + word_bit + 1));
-                    let next_bitpos =
-                        u64::try_from(self.word_pos).unwrap() * 64 + u64::from(word_bit + zeroes);
+                    assert!(zeroes + word_bit <= 64);
+                    if zeroes + word_bit <= 64 {
+                        self.current_word = Some((word, zeroes + word_bit + 1));
+                        let next_bitpos = u64::try_from(self.word_pos).unwrap() * 64
+                            + u64::from(word_bit + zeroes);
 
-                    Some(next_bitpos)
+                        return Some(next_bitpos);
+                    }
                 }
             }
-        } else {
-            None
+            self.current_word = None;
+            self.word_pos += 1;
         }
     }
 }
